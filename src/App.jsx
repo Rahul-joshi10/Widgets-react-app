d744224447d707538e07c7ba46541966f6eecb2d
Rahul joshi <joshirahul329@gmail.com>
2021-04-15 13:01:11 +0530

    Basic routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+
 
 const items = [
     {
@@ -17,13 +21,52 @@ const items = [
     }
 ]
 
-const widgets = () => {
+const options = [
+    {
+        label: 'None',
+        value: 'black'
+    },
+    {
+        label: 'Red',
+        value: 'red'
+    },
+    {
+        label: 'Green',
+        value: 'green'
+    },
+    {
+        label: 'Blue',
+        value: 'blue'
+    }
+]
+
+
+
+const Widgets = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            {/* <Accordion items={items} /> */}
-            <Search />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
+
         </div>
     );
 }
 
-export default widgets;
+export default Widgets;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,24 +1,71 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResults] = useState([]);
 
-    console.log("I run with every render")
+    //useEffect is same as lifeCycle method in class component.
     useEffect(() => {
-        console.log('I only run ')
-    }, []);
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term)
+        }, 1000);
+        return () => {
+            clearTimeout(timerId);//it invokes first when we change the input term
+            //to clear the previous timers
+        }
+    }, [term]);
+
+    useEffect(() => {
+        const search = async () => { //Defined this function because we can not give the argument function of useEffect as async function.
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm
+                },
+            });
+            setResults(data.query.search);
+        };
+        search();
+
+    }, [debouncedTerm]);
+
+
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>  {/*To remove the html we get from api req */}
+                </div>
+            </div>
+        );
+    })
 
     return (
         <div>
             <div className="ui form">
                 <div className="field">
-                    <label>Search</label>
+                    <label>Enter Search Term</label>
                     <input
                         className="input"
                         value={term}
                         onChange={e => setTerm(e.target.value)}
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
